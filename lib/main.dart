9c9e12829adb49a58a75122b7634fc686b09072d
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-06-16 02:49:03 +0530

    Added simulations page outline

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'src/menu.dart';
 
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simulate',
+      home: StartScreen(),
+    ));
+
+class StartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simulate'),
+        backgroundColor: Colors.red[500],
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('START'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MainMenu()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
