dfa21652dbf96aff24c41358ceb89b482a8781ab
Yash Lamba <yashlamba2000@gmail.com>
2019-06-16 04:03:09 +0530

    Added a new page for counting simulation

---
+++ lib/src/simulations/counting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class CountingHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counting'),
+        backgroundColor: Colors.red[500],
+      ),
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
