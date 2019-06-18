79320dfc8ef5fd2451a4aad6b109e379fde1dcc5
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-06-19 05:12:50 +0530

    Fixed errors caused due to absence of files
    The app name was changed mid-project, which caused an `import` statement to become redundant.
    The main class name was also changed from `MyApp` to `StartScreen`, which caused another problem.
    Fixed.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:simulate/main.dart';
+import 'package:Simulate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(StartScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
