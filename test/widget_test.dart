a43c3831cb8860f0c525a22fa810934e8ee9be56
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-07-02 01:08:35 +0530

    Fixed import statement

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Simulate/main.dart';
+import 'package:simulate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
