9d04ca129e33131573870579a202515406005ce5
Yash Lamba <yashlamba2000@gmail.com>
2019-07-02 04:19:19 +0530

    ...

--- lib/src/simulations/bubble_sort.dart
+++ lib/src/simulations/bubble_sort.dart
@@ -9,99 +9,19 @@ class BubbleSortBars extends StatefulWidget {
 }
 
 class _BubbleSortBarsState extends State<BubbleSortBars> {
-  int _numberOfElements = 0;
-  List<int> _elements;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Bubble Sort',
-          style: TextStyle(fontFamily: 'Ubuntu'),
-        ),
-        backgroundColor: Colors.teal,
-      ),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.teal,
-          child: Icon(Icons.play_arrow),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => BubbleSortSim(
-                      numberOfElements: _numberOfElements,
-                    ),
-              ),
-            );
-          }),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Text(
-                    'Number of Elements:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 4,
-                    child: TextField(
-                      maxLength: 4,
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        setState(() {
-                          _numberOfElements = int.parse(value);
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BubbleSortSim extends StatefulWidget {
-  final int numberOfElements;
-  BubbleSortSim({
-    Key key,
-    @required this.numberOfElements,
-  }) : super(key: key);
-  @override
-  _BubbleSortSimState createState() => _BubbleSortSimState();
-}
-
-class _BubbleSortSimState extends State<BubbleSortSim> {
+  int _numberOfElements = 12;
   List<int> _elements = [];
-  int i = 0;
+  int i = 0, counter = 0;
   int n;
   int tmp;
-  bool swap = true;
+  bool swap = false;
   double barwidth;
   List<Widget> containerList = [];
+  bool doNotRefresh = false;
 
   @override
   void initState() {
     super.initState();
-    var rng = new Random();
-    for (int i = 0; i < widget.numberOfElements; i++) {
-      _elements.add(rng.nextInt(400));
-    }
-    n = _elements.length;
   }
 
   @override
@@ -109,34 +29,44 @@ class _BubbleSortSimState extends State<BubbleSortSim> {
     super.dispose();
   }
 
-  List<Widget> returnContainers() {
+_containerList() {
+  containerList.clear();
+    if (!doNotRefresh) {
+      _elements.clear();
+      i = 0;
+      var rng = new Random();
+      for (int i = 0; i < _numberOfElements; i++) {
+        _elements.add(rng.nextInt(400));
+      }
+      n = _elements.length;
+    }
     this.barwidth = MediaQuery.of(context).size.width / (_elements.length + 1);
-    List<Widget> containers = [];
     for (int k = 0; k < _elements.length; ++k) {
       if (k == i) {
-        containers.add(Container(
+        containerList.add(Container(
           color: Colors.red,
           height: _elements[k] + 0.0,
           width: barwidth,
         ));
       } else if (k == i - 1) {
-        containers.add(Container(
+        containerList.add(Container(
           color: Colors.blue,
           height: _elements[k] + 0.0,
           width: barwidth,
         ));
       } else {
-        containers.add(Container(
+        containerList.add(Container(
           color: Colors.white,
           height: _elements[k] + 0.0,
           width: barwidth,
         ));
       }
     }
-    return containers;
   }
 
   nextStep() {
+    counter++;
+    setState(() {
     if (n == 1) {
       swap = false;
     }
@@ -151,21 +81,215 @@ class _BubbleSortSimState extends State<BubbleSortSim> {
       i++;
     } else {
       i++;
-    }
-    setState(() {
-      sleep(const Duration(seconds: 1));
-    });
+    }});
   }
 
   @override
   Widget build(BuildContext context) {
-    containerList = returnContainers();
+    _containerList();
     if (swap == true) {
       WidgetsBinding.instance.addPostFrameCallback((_) => nextStep());
     }
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: containerList,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Bubble Sort',
+          style: TextStyle(fontFamily: 'Ubuntu'),
+        ),
+        backgroundColor: Colors.teal,
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.teal,
+          child: Icon(Icons.play_arrow),
+          onPressed: () {
+            doNotRefresh = true;
+            swap = !swap;
+            setState(() {});
+            // doNotRefresh = false;
+          }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height / 8,
+        child: Material(
+          elevation: 30,
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              Spacer(flex: 2),
+              Slider(
+                min: 2,
+                max: 99,
+                onChanged: (value) {
+                  doNotRefresh = false;
+                  swap = false;
+                  setState(() {
+                    _numberOfElements = value.toInt();
+                  });
+                },
+                value: _numberOfElements.toDouble(),
+              ),
+              Center(
+                child: Text(
+                  "${_numberOfElements.toInt()}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Ubuntu',
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.grey[900],
+            child: Column(
+              children: <Widget>[
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: containerList,
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 5,
+            left: 5,
+            child: Text(
+              "Counter: $counter",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Ubuntu',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+// class BubbleSortSim extends StatefulWidget {
+//   final int numberOfElements;
+//   BubbleSortSim({
+//     Key key,
+//     @required this.numberOfElements,
+//   }) : super(key: key);
+//   @override
+//   _BubbleSortSimState createState() => _BubbleSortSimState();
+// }
+
+// class _BubbleSortSimState extends State<BubbleSortSim> {
+//   List<int> _elements = [];
+//   int i = 0;
+//   int n;
+//   int tmp;
+//   bool swap = true;
+//   double barwidth;
+//   List<Widget> containerList = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     var rng = new Random();
+//     for (int i = 0; i < widget.numberOfElements; i++) {
+//       _elements.add(rng.nextInt(400));
+//     }
+//     n = _elements.length;
+//   }
+
+//   @override
+//   dispose() {
+//     super.dispose();
+//   }
+
+//   List<Widget> returncontainerList() {
+//     this.barwidth = MediaQuery.of(context).size.width / (_elements.length + 1);
+//     List<Widget> containerList = [];
+//     for (int k = 0; k < _elements.length; ++k) {
+//       if (k == i) {
+//         containerList.add(Container(
+//           color: Colors.red,
+//           height: _elements[k] + 0.0,
+//           width: barwidth,
+//         ));
+//       } else if (k == i - 1) {
+//         containerList.add(Container(
+//           color: Colors.blue,
+//           height: _elements[k] + 0.0,
+//           width: barwidth,
+//         ));
+//       } else {
+//         containerList.add(Container(
+//           color: Colors.white,
+//           height: _elements[k] + 0.0,
+//           width: barwidth,
+//         ));
+//       }
+//     }
+//     return containerList;
+//   }
+
+//   nextStep() {
+//     if (n == 1) {
+//       swap = false;
+//     }
+//     if (i == n - 1) {
+//       i = 0;
+//       n--;
+//     }
+//     if (_elements[i] > _elements[i + 1]) {
+//       tmp = _elements[i];
+//       _elements[i] = _elements[i + 1];
+//       _elements[i + 1] = tmp;
+//       i++;
+//     } else {
+//       i++;
+//     }
+//     setState(() {
+//       sleep(const Duration(seconds: 1));
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     containerList = returncontainerList();
+//     // if (swap == true) {
+//     //   WidgetsBinding.instance.addPostFrameCallback((_) => nextStep());
+//     // }
+//     return Stack(
+//       children: <Widget>[
+//         Container(
+//           color: Colors.grey[900],
+//           child: Column(
+//             children: <Widget>[
+//               Spacer(),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: containerList,
+//               ),
+//               Spacer(),
+//             ],
+//           ),
+//         ),
+//         Positioned(
+//           top: 5,
+//           left: 5,
+//           child: Text(
+//             "Counter: ",
+//             style: TextStyle(
+//               color: Colors.white,
+//               fontFamily: 'Ubuntu',
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
