302e1df07ba78bbdffb841ce7b38014f4f8bd376
yashlamba <yashlamba2000@gmail.com>
2019-12-30 17:32:16 +0530

    AndroidX migration

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:simulate/src/data/themedata.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences sharedPreferences =
       await SharedPreferences.getInstance();
   runApp(
