9787891949526031780fc5d0601addc41d10554b
payal-98 <payalbudhiraja09@gmail.com>
2020-03-21 13:46:57 +0530

    'scrolling'

--- lib/src/simulations/lissajous_curve.dart
+++ lib/src/simulations/lissajous_curve.dart
@@ -4,6 +4,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/gestures.dart' show DragStartBehavior;
+import 'package:flutter/widgets.dart';
 
 GlobalKey<_LissajousState> globalKey = GlobalKey<_LissajousState>();
 
@@ -50,7 +52,8 @@ class _LissajousCurveState extends State<LissajousCurve> {
       allowFontScaling: true,
     );
     return Scaffold(
-      appBar: AppBar(
+      appBar: 
+      AppBar(
         automaticallyImplyLeading: false,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -106,7 +109,14 @@ class _LissajousCurveState extends State<LissajousCurve> {
           ),
         ),
       ),
-      bottomNavigationBar: Container(
+      
+      bottomNavigationBar: SingleChildScrollViewWithScrollbar(
+        scrollbarColor: Theme.of(context).accentColor.withOpacity(0.75),
+        scrollbarThickness: 8.0,
+        child:
+        Container(
+        height: 200,
+        child:Container(
         height: ScreenUtil().setHeight(1024/5),
         child: Material(
           elevation: 30,
@@ -195,7 +205,10 @@ class _LissajousCurveState extends State<LissajousCurve> {
           ),
         ),
       ),
-      body: Container(
+      ),
+      ),
+      body: 
+      Container(
         width: MediaQuery.of(context).size.width,
         child: Stack(
           children: <Widget>[
@@ -367,4 +380,125 @@ class LissajousPainter extends CustomPainter {
 
   @override
   bool shouldRebuildSemantics(LissajousPainter oldDelegate) => false;
+}
+
+class SingleChildScrollViewWithScrollbar extends StatefulWidget {
+  const SingleChildScrollViewWithScrollbar({
+    Key key,
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+    this.padding,
+    this.primary,
+    this.physics,
+    this.controller,
+    this.child,
+    this.dragStartBehavior = DragStartBehavior.down,
+    this.scrollbarColor,
+    this.scrollbarThickness = 6.0,
+  }) : super(key: key);
+
+  final Axis scrollDirection;
+  final bool reverse;
+  final EdgeInsets padding;
+  final bool primary;
+  final ScrollPhysics physics;
+  final ScrollController controller;
+  final Widget child;
+  final DragStartBehavior dragStartBehavior;
+  final Color scrollbarColor;
+  final double scrollbarThickness;
+
+  @override
+  _SingleChildScrollViewWithScrollbarState createState() => _SingleChildScrollViewWithScrollbarState();
+}
+
+class _SingleChildScrollViewWithScrollbarState extends State<SingleChildScrollViewWithScrollbar> {
+  AlwaysVisibleScrollbarPainter _scrollbarPainter;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    rebuildPainter();
+  }
+
+  @override
+  void didUpdateWidget(SingleChildScrollViewWithScrollbar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    rebuildPainter();
+  }
+
+  void rebuildPainter() {
+    final theme = Theme.of(context);
+    _scrollbarPainter = AlwaysVisibleScrollbarPainter(
+      color: widget.scrollbarColor ?? theme.highlightColor.withOpacity(1.0),
+      textDirection: Directionality.of(context),
+      thickness: widget.scrollbarThickness,
+    );
+  }
+
+ // @override
+  //void dispose() {
+    //_scrollbarPainter?.dispose();
+    //super.dispose();
+  //}
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: CustomPaint(
+        foregroundPainter: _scrollbarPainter,
+        child: RepaintBoundary(
+          child: SingleChildScrollView(
+            scrollDirection: widget.scrollDirection,
+            reverse: widget.reverse,
+            padding: widget.padding,
+            primary: widget.primary,
+            physics: widget.physics,
+            controller: widget.controller,
+            dragStartBehavior: widget.dragStartBehavior,
+            child: Builder(
+              builder: (BuildContext context) {
+                _scrollbarPainter.scrollable = Scrollable.of(context);
+                return widget.child;
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AlwaysVisibleScrollbarPainter extends ScrollbarPainter {
+  AlwaysVisibleScrollbarPainter({
+    @required Color color,
+    @required TextDirection textDirection,
+    @required double thickness,
+  }) : super(
+          color: color,
+          textDirection: textDirection,
+          thickness: thickness,
+          fadeoutOpacityAnimation: const AlwaysStoppedAnimation(1.0),
+        );
+
+  ScrollableState _scrollable;
+
+  ScrollableState get scrollable => _scrollable;
+
+  set scrollable(ScrollableState value) {
+    _scrollable?.position?.removeListener(_onScrollChanged);
+    _scrollable = value;
+    _scrollable?.position?.addListener(_onScrollChanged);
+    _onScrollChanged();
+  }
+
+  void _onScrollChanged() {
+    update(_scrollable.position, _scrollable.axisDirection);
+  }
+
+  @override
+  void dispose() {
+    _scrollable?.position?.removeListener(notifyListeners);
+    super.dispose();
+  }
 }
