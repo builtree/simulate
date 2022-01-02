ed7171a18b0dffa52e97c167f6449b9e49b9d541
Yash Lamba <yashlamba2000@gmail.com>
2022-01-02 22:13:33 +0530

    Updated packages: cupertino, url_launcher, shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:simulate/src/home.dart';
 import 'package:provider/provider.dart';
 import 'package:simulate/src/data/simulations.dart';
